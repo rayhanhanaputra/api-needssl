d8de642802298f8bf4a288611a947dcd5b0ff7eb
Rayhan Hanaputra <rayhanrhanaputra@outlook.com>
2022-06-28 14:00:30 +0700

    secure

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -45,7 +45,7 @@ exports.signin = (req, res) => {
   })
     .then(user => {
       if (!user) {
-        return res.status(404).send({ message: "User Not found." });
+        return res.status(404).send({ message: "Invalid password/user not found" });
       }
       var passwordIsValid = bcrypt.compareSync(
         req.body.password,
@@ -54,7 +54,7 @@ exports.signin = (req, res) => {
       if (!passwordIsValid) {
         return res.status(401).send({
           accessToken: null,
-          message: "Invalid Password!"
+          message: "Invalid password/user not found"
         });
       }
       var token = jwt.sign({ id: user.id }, config.secret, {
