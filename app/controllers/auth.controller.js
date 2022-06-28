f5e239a141d8a0cb979146f3e53597c581c40134
Rayhan Hanaputra <rayhanrhanaputra@outlook.com>
2022-06-28 14:18:37 +0700

    added login success

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -70,7 +70,8 @@ exports.signin = (req, res) => {
           username: user.username,
           email: user.email,
           roles: authorities,
-          accessToken: token
+          accessToken: token,
+          message: "login success",
         });
       });
       console.log(token);
